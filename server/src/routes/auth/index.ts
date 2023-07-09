7f888ca89b8bd5a4c0e784e9150d1f79fe3e35cc
Randy Morgan <digital.ipseity@gmail.com>
2023-07-10 01:26:22 +0900

    try non exclusive redirect

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -33,8 +33,8 @@ router.get('/github', passport.authenticate('github', { scope: ['profile'] }))
 router.get(
   '/github/callback',
   passport.authenticate('github', {
-    successRedirect: CLIENT_URL,
-    failureRedirect: CLIENT_URL,
+    successRedirect: '/',
+    failureRedirect: '/',
     // '/auth/login/failure',
   }),
 )
