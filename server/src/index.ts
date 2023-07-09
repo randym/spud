5e4fb7b281e3bbafc6d531e0bd40bb1377f0bb85
Randy Morgan <digital.ipseity@gmail.com>
2023-07-10 01:10:20 +0900

    try non-socket fixed port for host

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express'
 import os from 'os'
 import fs from 'fs'
-
+import io from 'socket.io'
 import { env } from './config'
 import { middleware } from './middleware'
 import { routes } from './routes'
 
 const app = express()
 
-const { HOST = os.hostname, PORT = 80, NODE_ENV = 'local' } = env
+const { HOST = os.hostname } = env
 
 middleware.apply(app)
 routes.apply(app)
@@ -23,13 +23,14 @@ app.use('/*', (req, rest) => {
 */
 // NGINX should forward all requests to anything non '/api/v1' to the client
 // distribution and reqwrite /api/v1 to here
+console.log('erm...', env.DYNO, env.NODE_ENV)
 
-app.listen(PORT, () => {
+app.listen(3000, () => {
   // Supposedly this will be set on heroku
   // perpaps there is a better way to check?
   if (env.DYNO) {
     console.log('This is running on Heroku')
     fs.openSync('/tmp/app-initialized', 'w')
   }
-  console.log(`Node app running on ${HOST}:${PORT}`)
+  console.log(`Node app running on ${HOST}:3000`)
 })
