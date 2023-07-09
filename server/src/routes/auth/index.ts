4165f0a812e70a6ca1e968dddc6b8d26b6213f8f
Randy Morgan <digital.ipseity@gmail.com>
2023-07-10 02:12:49 +0900

    https redirects

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -7,9 +7,26 @@ const { CLIENT_URL = 'http://localhost:5173' } = env
 
 const router = Router()
 
+router.get('/login/success', (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      success: true,
+      message: 'user has successfully authenticated',
+      user: req.user,
+    })
+  }
+})
+
+router.get('/login/failure', (_req, res) => {
+  res.status(401).json({
+    success: false,
+    message: 'user failed to authenticate.',
+  })
+})
+
 router.get('/logout', (req, res) => {
   req.logout(() => {})
-  res.redirect('/')
+  res.redirect(`https://${req.headers.host}/`)
 })
 
 router.get('/github', passport.authenticate('github', { scope: ['profile'] }))
@@ -20,6 +37,9 @@ router.get(
     successRedirect: '/',
     failureRedirect: '/',
   }),
+  (req, res) => {
+    res.redirect(`https://${req.headers.host}/`)
+  },
 )
 
 export const authRoutes = router
