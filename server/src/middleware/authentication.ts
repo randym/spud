fc067a7e27946b50f9c4ac2a494cc1943a5e7af0
Randy Morgan <digital.ipseity@gmail.com>
2023-07-10 01:35:27 +0900

    alternate callback

--- server/src/middleware/authentication.ts
+++ server/src/middleware/authentication.ts
@@ -13,7 +13,7 @@ const {
 const options: StrategyOptions = {
   clientID,
   clientSecret,
-  callbackURL: 'http://localhost:3000/auth/github/callback',
+  callbackURL: '/api/v1/auth/github/callback',
 }
 
 const isValidProfile = ({ username }: { username: string }) =>
