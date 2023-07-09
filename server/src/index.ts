daa0d0036952bf9f6ae6c4b7dd20253282d33ca3
Randy Morgan <digital.ipseity@gmail.com>
2023-07-10 01:15:42 +0900

    cleanup and fix nginx config typo

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,18 +12,6 @@ const { HOST = os.hostname } = env
 middleware.apply(app)
 routes.apply(app)
 
-/*
-const distPath = path.resolve('../../client/dist/')
-NODE_ENV !== 'local' && app.use(express.static(distPath))
-
-app.use('/*', (req, rest) => {
-  rest.redirect('/')
-})
-*/
-// NGINX should forward all requests to anything non '/api/v1' to the client
-// distribution and reqwrite /api/v1 to here
-console.log('erm...', env.DYNO, env.NODE_ENV)
-
 app.listen(3000, () => {
   // Supposedly this will be set on heroku
   // perpaps there is a better way to check?
