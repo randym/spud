d37e41e186c1a8772258ab26ece3bc3451d98693
Randy Morgan <digital.ipseity@gmail.com>
2023-07-11 01:00:18 +0900

    chore(env): use env vars for api server/port

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,17 @@
 import express from 'express'
-import os from 'os'
 import fs from 'fs'
 import { env } from './config'
 import { middleware } from './middleware'
 import { routes } from './routes'
 
 const app = express()
+const port = env.API_SERVCER_PORT || 3000
+const host = env.API_SERVER || 'localhost'
 
 middleware.apply(app)
 routes.apply(app)
 
-app.listen(3000, () => {
-  if (env.DYNO) {
-    fs.openSync('/tmp/app-initialized', 'w')
-  }
-  console.log(`Node app running on ${os.hostname}:3000`)
+app.listen(port, () => {
+  env.DYNO && fs.openSync('/tmp/app-initialized', 'w') // see heroku-buildback-nginx
+  console.log(`Node app running on ${host}:${port}`)
 })
